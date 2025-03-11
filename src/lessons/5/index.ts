14363f23054a9b6a1b79be450e5f4ef2a50d0ad2
Grzegorz Kielar <grzegorz.kielar@brand24.com>
2025-03-11 15:20:29 +0100

    Finish lesson 5

--- src/lessons/5/index.ts
+++ src/lessons/5/index.ts
@@ -1,8 +1,11 @@
-import { clearMessages, logMessage } from "../../index";
-import { from, fromEvent, interval, of, timer } from "rxjs";
+import {clearMessages, LOG_LEVEL_ERROR, logMessage} from "../../index";
+import {combineLatest, forkJoin, from, fromEvent, interval, Observable, of, timer} from "rxjs";
 
 const button = document.querySelector('button#clickMe')
 const clearButton = document.querySelector('button#clear')
+
+const temperatureInput = document.querySelector('input#temperatureInput')
+const convertType = document.querySelector('select#convertType')
 
 clearButton?.addEventListener('click', (event) => {
   clearMessages()
@@ -45,15 +48,60 @@ if (button) {
     subscriber.unsubscribe();
     logMessage('unsubscribed');
   }, 5000)
+}  else {
+  logMessage('Not found button element', LOG_LEVEL_ERROR)
 }
+
 logMessage('Waiting 2 sec...');
 timer(2000).subscribe({
   next: value => logMessage(`timer value: ${value}`),
   complete: () => logMessage('Completed timer'),
 })
 
-
-interval(1000).subscribe({
+const intervalSubscription$ =  interval(1000).subscribe({
   next: value => logMessage(`interval value: ${value}`),
   complete: () => logMessage('Completed timer'),
 })
+
+logMessage('Waiting 5 sec for stop interval');
+setTimeout(() => {
+  intervalSubscription$.unsubscribe();
+  logMessage('Interval unsubscribed');
+}, 5000);
+
+logMessage('Waiting 5 sec for success')
+const a$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.next('A');
+      subscriber.complete()
+  }, 5000);
+  return () => logMessage('A teardown')
+})
+
+logMessage('Waiting 3 sec for failure')
+const b$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error('Failure')
+  }, 3000);
+  return () => logMessage('B teardown')
+})
+
+// All subscriptions will be completed if an error occurs
+forkJoin([a$, b$]).subscribe({
+  next: ([a, b]) => logMessage(`Value from A: ${a} Value from B: ${b}`),
+  error: (error) => logMessage(error, LOG_LEVEL_ERROR)
+})
+
+if (temperatureInput && convertType) {
+  const temperatureInputObservable$ = fromEvent<InputEvent>(temperatureInput, 'input');
+  const convertTypeObservable$ = fromEvent(convertType, 'input');
+
+  // Will run only if all observables emit value at least once
+  combineLatest([temperatureInputObservable$, convertTypeObservable$]).subscribe({
+    next: ([tempInput, typeInput]) => {
+      logMessage(`Value: ${(tempInput.target as HTMLInputElement).value} Type: ${(typeInput.target as HTMLSelectElement).value}`)
+    }
+  })
+} else {
+  logMessage('Not found converted elements', LOG_LEVEL_ERROR)
+}
